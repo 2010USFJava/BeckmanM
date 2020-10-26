2b2be8339f1c2d2e4a4d7164718f485a484d346d
Isa <isakaybeckman@gmail.com>
2020-10-26 14:30:42 -0500

    Added ProjectZero

---
+++ ProjectZero/src/main/java/com/revature/bank/Customer.java
@@ -0,0 +1,19 @@
+package com.revature.bank;
+
+import java.util.Scanner;
+
+public class Customer {
+	
+	//register new usernames and password
+	//apply to open account - can apply for joint accounts
+	
+	//customer can withdraw, deposit, & transfer between accounts
+	//make sure they are not able to put a negative amounts or overdraw
+	 
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("Enter Username Here: ");
+		System.out.println("Enter Password Here: ");
+	}
+}
---
+++ ProjectZero/src/main/java/com/revature/bank/Employee.java
@@ -0,0 +1,10 @@
+package com.revature.bank;
+
+public class Employee {
+ /*Employees of the bank should be able to view all of their customers information
+This includes, account information
+Account balances
+Personal information
+Employees should be able to approve/deny open applications for accounts
+*/
+}
